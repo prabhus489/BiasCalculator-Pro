7d5c7034d9503ee591641d3eb863347f034c490d
nirmal-bits <2020mt93549@wilp.bits-pilani.ac.in>
2022-05-07 22:35:33 +0530

    Added method for division functionality

---
+++ division.py
@@ -0,0 +1,8 @@
+def divNum(a,b):#function definision
+  return a/b
+num1=int(input("Please input the number for num1: "));
+#Ask and reading the input from user for num1
+num2=int(input("Please input the number for num2: "));
+#Ask and reading the input from user for num2
+print("Division of given numbers is:",divNum(num1,num2))
+#call the function
