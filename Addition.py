68308599aa74e5b58c784a60324936419e5031dd
nirmal-bits <2020mt93549@wilp.bits-pilani.ac.in>
2022-05-07 23:56:26 +0530

    enhanced addition functionality

--- Addition.py
+++ Addition.py
@@ -1,10 +1,9 @@
-# To add all numbers in a collection
-def AddCollection(inputValues):
-    sum = 0    
-    for num in inputValues:
-        sum +=num
-    return sum
+#Python program to add two numbers using function
 
-# To add two numbers
-def AddTwoNumbers(num1, num2):
-    return num1 + num2
+def add_num(a,b):#function for addition
+    sum=a+b;
+    return sum; #return value
+num1=int(input("input the number one: "))#input from user for num1
+num2=int(input("input the number one :"))#input from user for num2
+
+print("The sum is",add_num(num1,num2))#call te function
